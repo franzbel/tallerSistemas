5565c942ca955077c47ebc085a19c8cf08e6c86a
irmp <hola>
2014-10-21 11:56:18 -0400

    diferent users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,17 @@
 class WelcomeController < ApplicationController
   def index
+  		if user_signed_in? && current_user.email == 'chef@gmail.com'
+     	 redirect_to '/chefs/index'
+        end
+        if user_signed_in? && current_user.email == 'mesero@gmail.com'
+     	 redirect_to '/waiters/index'
+        end
+        if user_signed_in? && current_user.email == 'barman@gmail.com'
+     	 redirect_to '/bartenders/index'
+        end
+        if user_signed_in? && current_user.email == 'ayudante@gmail.com'
+     	 redirect_to '/helpers/index'
+        end
   end
+
 end

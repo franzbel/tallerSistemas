947ff12c1fd6ed8586c6220ac858c57caa46088e
irmp <hola>
2014-10-21 15:20:27 -0400

    roles

---
+++ db/migrate/20141021191259_add_rolee_to_user.rb
@@ -0,0 +1,5 @@
+class AddRoleeToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :role, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021145717) do
+ActiveRecord::Schema.define(version: 20141021191259) do
 
   create_table "tables", force: true do |t|
     t.string   "status"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20141021145717) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

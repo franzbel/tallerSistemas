26e94b7e082d9c5c301dd38d81eefa7ae675e690
irmp <hola>
2014-10-21 16:15:53 -0400

    change rols

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -1,4 +1,45 @@
 class ManagersController < ApplicationController
   def index
   end
+  def cuentas
+    @users = User.all
+  end
+
+  def role_admi
+    @user=User.find(params[:id])
+    @user.role = 'admin'
+    @user.save
+    redirect_to :back
+  end
+
+  def role_asistente
+    @user=User.find(params[:id])
+    @user.role = 'asistente'
+    @user.save
+    redirect_to :back
+  end
+
+  def role_barman
+    @user=User.find(params[:id])
+    @user.role = 'barman'
+    @user.save
+    redirect_to :back
+  end
+
+  def role_mesero
+    @user=User.find(params[:id])
+    @user.role = 'mesero'
+    @user.save
+    redirect_to :back
+  end
+
+def role_chef
+    @user=User.find(params[:id])
+    @user.role = 'chef'
+    @user.save
+    redirect_to :back
+  end
+
+
+
 end

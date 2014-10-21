02e71420481b4c48dbafd2480906cd8299a7fb73
Franz Beltran <franzbeltran79@gmail.com>
2014-10-21 10:13:54 -0400

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fogondemaria::Application.config.secret_key_base = 'd65f7120680abc0e5263a2815fc552c5d96d39d0f4e9517829d84eed801282c4afe49b5bcd8ce56ea2e313682954f8b8128f3ed3a128612c012e9604f8242709'

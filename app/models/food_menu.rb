ef11c39638b1b7dca7d06d588327da3320079da6
leslicita_mc@hotmail.com <leslicita_mc@hotmail.com>
2014-10-22 10:05:43 -0400

    Validaciones en DrinkMenu y FoodMenu

--- app/models/food_menu.rb
+++ app/models/food_menu.rb
@@ -1,3 +1,13 @@
 class FoodMenu < ActiveRecord::Base
 	has_and_belongs_to_many :orders
+
+	validates :name, presence: {:message => "- El nombre de la comida es un campo obligatorio"}
+	validates :name, uniqueness: {case_sensitive: false, :message => "- La bebida ya existe"}
+
+	validates :description, presence: {:message => "- La descripcion es un campo obligatorio"}
+
+	validates :price, presence: {:message => "- El precio es un campo obligatorio"}
+	validates :price, numericality: {:message => "- El valor del precio debe ser numerico"}
+	validates :price, length: {minimum: 2, maximum: 3, :message => "- El precio debe tener entre 2 a 3 digitos"}
+	validates :price, numericality: {greater_than: 0, :message => "- El precio no puede ser negativo"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,11 +37,12 @@ ActiveRecord::Schema.define(version: 20141022065916) do
   end
 
   create_table "food_menus_orders", id: false, force: true do |t|
-      t.integer "food_menus_id"
+    t.integer "food_menus_id"
     t.integer "orders_id"
     t.integer "food_menu_id"
     t.integer "order_id"
   end
+
   create_table "ingredients", force: true do |t|
     t.string   "ingrediente"
     t.integer  "recipe_id"
@@ -67,7 +68,6 @@ ActiveRecord::Schema.define(version: 20141022065916) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "tables", force: true do |t|
     t.string   "state"
